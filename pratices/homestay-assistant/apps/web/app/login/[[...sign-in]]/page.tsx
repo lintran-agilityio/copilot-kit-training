60bddaa7fa70b0bfbca5b850464a174b67e01f57
Lin Tran <lin.tran@asnet.com.vn>
2026-06-22 18:06:29 +0700

    Fix warning for clerk route

---
+++ pratices/homestay-assistant/apps/web/app/login/[[...sign-in]]/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+// Libs
+import { SignIn } from "@clerk/nextjs";
+
+import { ROUTES } from "@/constants";
+
+const LoginPage = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col items-center justify-center">
+        <SignIn routing="path" path={ROUTES.LOGIN} />
+      </div>
+    </main>
+  )
+};
+
+export default LoginPage;
