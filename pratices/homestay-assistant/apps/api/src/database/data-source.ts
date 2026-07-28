948cd9ec1a63d1104f1fccacc0e5cbafa88f2fdb
Lin Tran <lin.tran@asnet.com.vn>
2026-07-28 10:23:56 +0700

    Update command for build api

--- pratices/homestay-assistant/apps/api/src/database/data-source.ts
+++ pratices/homestay-assistant/apps/api/src/database/data-source.ts
@@ -1,12 +1,19 @@
 import 'reflect-metadata';
 import 'dotenv/config';
 import { DataSource } from 'typeorm';
+import { join } from 'node:path';
 import { databaseEntities } from './entities';
+
+const isTs = __filename.endsWith('.ts');
 
 export default new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
   entities: databaseEntities,
-  migrations: ['src/database/migrations/*.ts'],
+  migrations: [
+    isTs
+      ? join(__dirname, 'migrations', '*.ts')
+      : join(__dirname, 'migrations', '*.js'),
+  ],
   synchronize: false,
 });
